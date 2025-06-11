97bfb02464cdb0cd906b4d7b2c91ea1979acb974
Muammer Cengiz <muammercengiz@aof.anadolu.edu.tr>
2025-06-11 19:11:05 +0300

    Render bağlantısı için veritabanı config ayarlandı

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,15 @@
-// backend/db.js
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "aslanpanel",
-  password: "0044",
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
